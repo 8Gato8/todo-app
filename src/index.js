1be9269d3b8defe6df1431130c9f36b38a421814
Sergey Scotcelias <sergey.sckotcelias@yandex.ru>
2024-10-04 08:53:50 +0800

    feat: connect taskEditor module to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.css';
+
+let activeProject = 'income';
+
+import taskEditor from './js/dom/taskEditor';
+
+/* import generateUniqueId from './generateUniqueId';
+
+
+import createProject from './project';
+
+const tasks = [];
+
+const brush = createTask({
+  title: 'Купить щётку',
+  dueDate: '29.08.2024',
+  priority: 'low',
+  project: sport,
+  id: 1,
+});
+
+brush.project.addTask(brush); */
