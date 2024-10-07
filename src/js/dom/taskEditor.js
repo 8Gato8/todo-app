00e3073b04479c47a1a7744dc56919e2b1ba0b80
Sergey Scotcelias <sergey.sckotcelias@yandex.ru>
2024-10-07 17:00:49 +0800

    feat: add functionality for closing popups

--- src/js/dom/taskEditor.js
+++ src/js/dom/taskEditor.js
@@ -1,22 +1,26 @@
+import {
+  TASK_EDITOR_CLASS_FOR_VISIBLE_STATE,
+  CHOICE_POPUP_CLASS_FOR_VISIBLE_STATE,
+} from '../variables';
+
 /* query selectors */
 
 const sidebarOpenTaskEditorButton = document.querySelector('.sidebar__open-task-editor-button');
 
-const taskEditorContainer = document.querySelector('.task-editor-container');
+const taskEditorOverlay = document.querySelector('.task-editor-overlay');
 
+const projectChoiceContainer = document.querySelector('#project-choice-container');
 const chooseProjectButton = document.querySelector('#choose-project-button');
 const projectChoicePopup = document.querySelector('#project-choice-popup');
 
+const priorityChoiceContainer = document.querySelector('#priority-choice-container');
 const choosePriorityButton = document.querySelector('#choose-priority-button');
 const priorityChoicePopup = document.querySelector('#priority-choice-popup');
 
+const choicePopupsNodeList = document.querySelectorAll('.popup-button__popup');
+
 const cancelButton = document.querySelector('#cancel-button');
-const addTaskButton = document.querySelector('#add-task-button');
-
-/* constants */
-
-const TASK_EDITOR_CLASS_FOR_VISIBLE_STATE = 'task-editor-container_visible';
-const CHOICE_POPUP_CLASS_FOR_VISIBLE_STATE = 'popup-button__popup_visible';
+/* const addTaskButton = document.querySelector('#add-task-button'); */
 
 /* utils */
 
@@ -30,10 +34,30 @@ function closePopup(popup, classForVisibleState) {
   popup.classList.remove(classForVisibleState);
 }
 
+function anyChoicePopupOpen(choicePopupsNodeList, classForVisibleState) {
+  const choicePopupsArray = Array.from(choicePopupsNodeList);
+  return choicePopupsArray.some((choicePopup) => isPopupOpen(choicePopup, classForVisibleState));
+}
+
+function makeElementChildrenList(elementNode) {
+  const listOfChildren = [];
+
+  if (elementNode.children.length === 0) return elementNode;
+
+  for (let child of elementNode.children) {
+    listOfChildren.push(child, makeElementChildrenList(child));
+  }
+
+  return listOfChildren.flat(Infinity);
+}
+
+const projectPopupContainerChildrenList = makeElementChildrenList(projectChoiceContainer);
+const priorityPopupContainerChildrenList = makeElementChildrenList(priorityChoiceContainer);
+
 /* handlers */
 
 function handelSidebarOpenTaskEditorButtonClick() {
-  openPopup(taskEditorContainer, TASK_EDITOR_CLASS_FOR_VISIBLE_STATE);
+  openPopup(taskEditorOverlay, TASK_EDITOR_CLASS_FOR_VISIBLE_STATE);
 }
 
 function handleProjectChoicePopupButtonClick() {
@@ -44,35 +68,64 @@ function handlePriorityChoicePopupButtonClick() {
   openPopup(priorityChoicePopup, CHOICE_POPUP_CLASS_FOR_VISIBLE_STATE);
 }
 
-function handleClickOutsidePopup(e, classForVisibleState) {
+function handleTaskEditorOverlayClick(
+  e,
+  taskEditorClassForVisibleState,
+  choicePopupClassForVisibleState,
+) {
   const popup = e.currentTarget;
   const target = e.target;
 
-  /* TODO: Доработать логику для закрытия попапов выбора проекта и приоритета */
-
-  if (target === popup && isPopupOpen(popup, classForVisibleState)) {
-    closePopup(popup, classForVisibleState);
+  if (
+    target === popup &&
+    !anyChoicePopupOpen(choicePopupsNodeList, choicePopupClassForVisibleState)
+  ) {
+    closePopup(popup, taskEditorClassForVisibleState);
   }
 }
 
+function handleClickOutsideChoicePopup(e, popup, popupContainerChildrenList, classForVisibleState) {
+  if (!isPopupOpen(popup, classForVisibleState)) return;
+
+  const target = e.target;
+
+  if (popupContainerChildrenList.some((child) => child === target)) return;
+
+  closePopup(popup, classForVisibleState);
+}
+
 function handleCancleButtonClick() {
-  closePopup(taskEditorContainer, TASK_EDITOR_CLASS_FOR_VISIBLE_STATE);
+  closePopup(taskEditorOverlay, TASK_EDITOR_CLASS_FOR_VISIBLE_STATE);
 }
 
 /* event listeners */
 
 sidebarOpenTaskEditorButton.addEventListener('click', handelSidebarOpenTaskEditorButtonClick);
 
-taskEditorContainer.addEventListener('click', (e) =>
-  handleClickOutsidePopup(e, TASK_EDITOR_CLASS_FOR_VISIBLE_STATE),
+taskEditorOverlay.addEventListener('click', (e) =>
+  handleTaskEditorOverlayClick(
+    e,
+    TASK_EDITOR_CLASS_FOR_VISIBLE_STATE,
+    CHOICE_POPUP_CLASS_FOR_VISIBLE_STATE,
+  ),
 );
 
-projectChoicePopup.addEventListener('click', (e) =>
-  handleClickOutsidePopup(e, CHOICE_POPUP_CLASS_FOR_VISIBLE_STATE),
+document.addEventListener('click', (e) =>
+  handleClickOutsideChoicePopup(
+    e,
+    projectChoicePopup,
+    projectPopupContainerChildrenList,
+    CHOICE_POPUP_CLASS_FOR_VISIBLE_STATE,
+  ),
 );
 
-priorityChoicePopup.addEventListener('click', (e) =>
-  handleClickOutsidePopup(e, CHOICE_POPUP_CLASS_FOR_VISIBLE_STATE),
+document.addEventListener('click', (e) =>
+  handleClickOutsideChoicePopup(
+    e,
+    priorityChoicePopup,
+    priorityPopupContainerChildrenList,
+    CHOICE_POPUP_CLASS_FOR_VISIBLE_STATE,
+  ),
 );
 
 chooseProjectButton.addEventListener('click', handleProjectChoicePopupButtonClick);
