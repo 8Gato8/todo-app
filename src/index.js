09e3a4010a8f74d6a53083a1d014427a7d0e5895
Sergey Scotcelias <sergey.sckotcelias@yandex.ru>
2024-10-16 15:51:36 +0800

    feat: add draft version of index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 import './style.css';
 
-let activeProject = 'income';
-
 import taskEditor from './js/dom/taskEditor';
 
-/* import generateUniqueId from './generateUniqueId';
+import createTask from './js/task';
+import createProject from './js/project';
 
+import generateUniqueId from './js/utils/generateUniqueId';
 
-import createProject from './project';
+export const priorities = [
+  { title: 'Приоритет 1', number: 1 },
+  { title: 'Приоритет 2', number: 2 },
+  { title: 'Приоритет 3', number: 3 },
+  { title: 'Приоритет 4', number: 4 },
+];
+export const projects = [];
 
-const tasks = [];
+function createProjectWithUniqueId(newProjectData) {
+  const id = generateUniqueId();
 
-const brush = createTask({
-  title: 'Купить щётку',
-  dueDate: '29.08.2024',
-  priority: 'low',
-  project: sport,
-  id: 1,
-});
+  const newProject = createProject({ ...newProjectData, id });
+  return newProject;
+}
 
-brush.project.addTask(brush); */
+export function createTaskWithUniqueId(newTaskData) {
+  const id = generateUniqueId();
+
+  const newTask = createTask({ ...newTaskData, id });
+
+  return newTask;
+}
+
+export function addTaskToProject(newTask) {
+  newTask.project.addTask(newTask);
+}
+
+export const inboxProject = createProjectWithUniqueId({ title: 'Входящие' });
+
+projects.push(inboxProject);
+
+/* adds all sorts of event listeners related to task editor */
+taskEditor();
