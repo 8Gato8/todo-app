5bbc1d14b39552c91fe7b6acf6b8aa36ae605b5f
Sergey Scotcelias <sergey.sckotcelias@yandex.ru>
2024-10-17 20:39:42 +0800

    feat: add variables for project project nav styles

--- src/js/dom/projectNavigation/variables.js
+++ src/js/dom/projectNavigation/variables.js
@@ -1,2 +1,5 @@
 export const PROJECT_NAVIGATION_BUTTON_CLASS_FOR_HIGHLIGHTED_STATE =
   'project-navigation__button_active';
+
+export const PROJECT_NAVIGATION_CHEVRON_BUTTON_OPEN = 'project-navigation__chevron-button_open';
+export const PROJECT_NAVIGATION_LIST_ITEM_HIDDEN = 'project-navigation-list__item_hidden';
