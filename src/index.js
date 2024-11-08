048a8794ee090b95af2227d136aaa198b5c56783
Sergey Scotcelias <sergey.sckotcelias@yandex.ru>
2024-11-08 21:11:28 +0800

    feat: add placeholder colors to data objects

--- src/index.js
+++ src/index.js
@@ -9,12 +9,59 @@ import createProject from './js/project';
 import generateUniqueId from './js/utils/generateUniqueId';
 
 export const priorities = [
-  { title: 'Приоритет 1', number: 1 },
-  { title: 'Приоритет 2', number: 2 },
-  { title: 'Приоритет 3', number: 3 },
-  { title: 'Приоритет 4', number: 4 },
+  {
+    title: 'Приоритет 1',
+    number: 1,
+    color: {
+      title: 'Ягодно-красный',
+      hexCode: '#b8255f',
+    },
+  },
+  {
+    title: 'Приоритет 2',
+    number: 2,
+    color: {
+      title: 'Красный',
+      hexCode: '#cf473a',
+    },
+  },
+  {
+    title: 'Приоритет 3',
+    number: 3,
+    color: {
+      title: 'Оранжевый',
+      hexCode: '#c77100',
+    },
+  },
+  {
+    title: 'Приоритет 4',
+    number: 4,
+    color: {
+      title: 'Желтый',
+      hexCode: '#b29104',
+    },
+  },
 ];
 export const projects = [];
+
+export const colors = [
+  {
+    title: 'Ягодно-красный',
+    hexCode: '#b8255f',
+  },
+  {
+    title: 'Красный',
+    hexCode: '#cf473a',
+  },
+  {
+    title: 'Оранжевый',
+    hexCode: '#c77100',
+  },
+  {
+    title: 'Желтый',
+    hexCode: '#b29104',
+  },
+];
 
 function createProjectWithUniqueId(newProjectData) {
   const id = generateUniqueId();
@@ -36,10 +83,16 @@ export function addTaskToProject(newTask) {
 }
 
 export const inboxProject = createProjectWithUniqueId({ title: 'Входящие' });
-const homeProject = createProjectWithUniqueId({ title: 'Дом' });
-const jobProject = createProjectWithUniqueId({ title: 'Работа' });
+const homeProject = createProjectWithUniqueId({
+  title: 'Дом',
+  color: { title: 'Красный', hexCode: '#cf473a' },
+});
+const jobProject = createProjectWithUniqueId({
+  title: 'Работа',
+  color: { title: 'Желтый', hexCode: '#b29104' },
+});
 
-projects.push(inboxProject);
+projects.push(inboxProject, homeProject, jobProject);
 
 /* adds all sorts of event listeners related to task editor */
 projectNavigation();
